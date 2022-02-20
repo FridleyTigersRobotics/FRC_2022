8fd8d5cbbb7dc48d7665f398f9ef0e5765004e35
Terrrm <9992188+Terrrm@users.noreply.github.com>
2022-02-19 21:49:59 -0600

    Initial Commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,398 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+
+#include <fmt/core.h>
+
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/DigitalInput.h>
+
+
+void Robot::RobotInit() {
+
+  limelightNetworkTable = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+  //bool   targetValid                  = limelightNetworkTable->GetNumber("tv",false);
+  //double targetOffsetAngle_Horizontal = limelightNetworkTable->GetNumber("tx",0.0);
+  //double targetOffsetAngle_Vertical   = limelightNetworkTable->GetNumber("ty",0.0);
+  //double targetArea                   = limelightNetworkTable->GetNumber("ta",0.0);
+  //double targetSkew                   = limelightNetworkTable->GetNumber("ts",0.0);
+
+  m_wheelFrontLeft.SetInverted( true );
+  m_wheelRearLeft.SetInverted( true );
+
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+  // Does not seem to apply to the rotation...
+  m_drive.SetDeadband(0.3);
+
+  // TODO : Update these values
+  m_rotatePid.SetTolerance(5.0, 10);
+  m_rotatePid.SetIntegratorRange(-0.6, 0.6);
+
+  m_shooter.RestoreFactoryDefaults();
+  m_pidController.SetP( 6e-5 );
+  m_pidController.SetI( 0.0 );
+  m_pidController.SetD( 0.0 );
+  m_pidController.SetIZone( 0.0 );
+  m_pidController.SetFF( 0.00025 );
+  m_pidController.SetOutputRange( -1.0, 1.0 );
+
+  m_shooterAngle.ConfigFactoryDefault();
+
+  /* Configure Sensor Source for Pirmary PID */
+  m_shooterAngle.ConfigSelectedFeedbackSensor(
+    FeedbackDevice::CTRE_MagEncoder_Relative,
+    0, 
+    10
+  );
+
+  /**
+   * Configure Talon SRX Output and Sensor direction accordingly
+   * Invert Motor to have green LEDs when driving Talon Forward / Requesting Postiive Output
+   * Phase sensor to have positive increment when driving Talon Forward (Green LED)
+   */
+  m_shooterAngle.SetSensorPhase(false);
+  m_shooterAngle.SetInverted(false);
+
+  /* Set relevant frame periods to be at least as fast as periodic rate */
+  m_shooterAngle.SetStatusFramePeriod(StatusFrameEnhanced::Status_13_Base_PIDF0, 10, 10);
+  m_shooterAngle.SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic, 10, 10);
+
+  /* Set the peak and nominal outputs */
+  m_shooterAngle.ConfigNominalOutputForward(0, 10);
+  m_shooterAngle.ConfigNominalOutputReverse(0, 10);
+  m_shooterAngle.ConfigPeakOutputForward(0.8, 10);
+  m_shooterAngle.ConfigPeakOutputReverse(-0.8, 10);
+
+  /* Set Motion Magic gains in slot0 - see documentation */
+  m_shooterAngle.SelectProfileSlot(0, 0);
+  m_shooterAngle.Config_kF(0, 0.5, 10);
+  m_shooterAngle.Config_kP(0, 0.5, 10);
+  m_shooterAngle.Config_kI(0, 0.0001, 10);
+  m_shooterAngle.Config_kD(0, 0.0, 10);
+
+  /* Set acceleration and vcruise velocity - see documentation */
+  m_shooterAngle.ConfigMotionCruiseVelocity(25000, 10);
+  m_shooterAngle.ConfigMotionAcceleration(25000, 10);
+
+  /* Zero the sensor */
+  m_shooterAngle.SetSelectedSensorPosition(0, 0, 10);
+
+}
+
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want ran during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  fmt::print("Auto selected: {}\n", m_autoSelected);
+
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::AutonomousPeriodic() {
+
+  // Calibrate Hood Angle.
+  if ( !m_hoodAngleCalFinished )
+  {
+    CalibrateShooterAngle();
+  }
+
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::TeleopInit() {
+
+}
+
+// TODO : Check signs of all motors.
+
+void Robot::TeleopPeriodic() {
+  bool targetValid = limelightNetworkTable->GetNumber("tv",false);
+
+  // Cannot aim until hood angle calibration has finished.
+  bool Aiming        = m_driverController.GetAButton() && m_hoodAngleCalFinished;
+  bool intakeEnabled = m_driverController.GetRightBumper();
+
+  // Limelight Control
+  // nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("ledMode",<value>);
+  // limelightNetworkTable->PutNumber("ledMode",<value>);
+  //  0	use the LED Mode set in the current pipeline
+  //  1	force off
+  //  2	force blink
+  //  3	force on
+  if ( Aiming )
+  {
+    limelightNetworkTable->PutNumber( "ledMode", 0 );
+  }
+  else
+  {
+    limelightNetworkTable->PutNumber( "ledMode", 1 );
+  }
+
+
+  // Drive system Control
+  if ( Aiming )
+  {
+    double const ySpeed       = m_driverController.GetLeftY();
+    double const xSpeed       = -m_driverController.GetLeftX();
+    double const targetXValue = limelightNetworkTable->GetNumber( "tx", 0.0 );
+    double const rotation     = m_rotatePid.Calculate( targetXValue, 0.0 );
+    m_drive.DriveCartesian( ySpeed, xSpeed, rotation );
+  }
+  else
+  {
+    double const ySpeed   = m_driverController.GetLeftY();
+    double const xSpeed   = -m_driverController.GetLeftX();
+    double       rotation = -m_driverController.GetRightX();
+
+    // Deadband does not apply to rotation for some reason...
+    if ( fabs( rotation ) < 0.3 )
+    {
+      rotation = 0.0;
+    }
+
+    m_drive.DriveCartesian( ySpeed, xSpeed, rotation );
+  }
+
+
+  // Shooter Control
+  if ( Aiming )
+  {
+    // TODO : Determine Velocity to use.
+    m_pidController.SetReference( -1500, rev::ControlType::kVelocity );
+  }
+  else
+  {
+    m_pidController.SetReference( 0, rev::ControlType::kVelocity );
+  }
+  frc::SmartDashboard::PutNumber( "Shooter Speed:",  m_encoder.GetVelocity() );
+  frc::SmartDashboard::PutNumber( "Shooter Output:", m_shooter.GetAppliedOutput() );
+
+
+  // Hood Angle
+  if ( m_hoodAngleCalFinished )
+  {
+    double targetYValue         = limelightNetworkTable->GetNumber( "ty", 0.0 );
+    double shooterAnglePosition = DetermineShooterAngleFromTargetPosition( targetYValue );
+    ChangeShooterAngle( shooterAnglePosition );
+  }
+  else
+  {
+    CalibrateShooterAngle();
+  }
+  frc::SmartDashboard::PutNumber("Hood Output",   m_shooterAngle.GetMotorOutputPercent() );
+  frc::SmartDashboard::PutNumber("Hood Velocity", m_shooterAngle.GetSelectedSensorVelocity() );
+  frc::SmartDashboard::PutNumber("Hood Position", m_shooterAngle.GetSelectedSensorPosition() );
+  frc::SmartDashboard::PutNumber("Hood error   ", m_shooterAngle.GetClosedLoopError(0) );
+
+
+  // Intake Control
+  if ( intakeEnabled ) 
+  {
+    m_intakeSpin.Set( ControlMode::PercentOutput, -1 );
+    MoveIntakeOut();
+  }
+  else
+  {
+    m_intakeSpin.Set( ControlMode::PercentOutput, 0.0 );
+    MoveIntakeIn();
+
+    // Pull balls into indexer when intake is moving inward.
+    if ( IntakeMovingInward() && 
+         !IsBallDetected() )
+    {
+      m_indexer.Set(-0.5 );
+    }
+    else
+    {
+      m_indexer.Set( 0.0 );
+    }
+  }
+
+
+  // Indexer Control
+  if ( Aiming )
+  {
+    m_indexer.Set( -m_driverController.GetRightTriggerAxis() );
+  }
+
+
+  // Climber Control
+  if ( m_driverController.GetXButton() )
+  {
+    m_climber.Set( ControlMode::PercentOutput, 1.0 );
+  }
+  else if ( m_driverController.GetYButton() )
+  {
+    m_climber.Set( ControlMode::PercentOutput, -1.0 );
+  } 
+  else
+  {
+    m_climber.Set( ControlMode::PercentOutput, 0.0 );
+  }
+}
+
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+
+void Robot::TestPeriodic() {
+  // TEST Controls:
+  //   Intake Arm   = Bumpers
+  //   Indexer      = Right Stick
+  //   Shooter Hood = Triggers
+  
+  // WARNING, no limits are enabled in this mode.
+
+  m_indexer.Set( m_driverController.GetRightY() );
+
+  double intakeSpeed = ( m_driverController.GetRightBumper() ? ( 1.0 ) : ( 0.0 ) ) -
+                       ( m_driverController.GetLeftBumper()  ? ( 1.0 ) : ( 0.0 ) );
+  m_intakeMove.Set( intakeSpeed );
+
+  double hoodSpeed = m_driverController.GetRightTriggerAxis() -
+                     m_driverController.GetLeftTriggerAxis();
+  m_shooterAngle.Set( TalonSRXControlMode::PercentOutput, hoodSpeed * 0.2 );
+
+  if ( m_driverController.GetAButton() )
+  {
+    m_intakeSpin.Set( ControlMode::PercentOutput, -1 );
+  }
+  else
+  {
+    m_intakeSpin.Set( ControlMode::PercentOutput, 0 );
+  }
+
+  if ( m_driverController.GetXButton() )
+  {
+    m_climber.Set( ControlMode::PercentOutput, 1.0 );
+  }
+  else if ( m_driverController.GetYButton() )
+  {
+    m_climber.Set( ControlMode::PercentOutput, -1.0 );
+  } 
+  else
+  {
+    m_climber.Set( ControlMode::PercentOutput, 0.0 );
+  }
+}
+
+// Intake
+void Robot::SetIntakeMoveSpeed( double speed ) {
+  bool intake_OutStop = !m_intakeOutStop.Get();
+  bool intake_InStop  = !m_intakeInStop.Get();
+
+  if ( ( speed < 0 && intake_InStop ) ||
+       ( speed > 0 && intake_OutStop ) )
+  {
+    m_intakeMove.Set( 0 );
+  }
+  else
+  {
+    m_intakeMove.Set( speed );
+  }
+}
+
+void Robot::MoveIntakeOut() {
+  SetIntakeMoveSpeed( 1.0 );
+}
+
+void Robot::MoveIntakeIn() {
+  SetIntakeMoveSpeed( -1.0 );
+}
+
+bool Robot::IntakeMovingInward() { 
+  return m_intakeMove.Get() < 0.0;
+}
+
+
+bool Robot::IsBallDetected() {
+  // TODO : update this value for th sensor.
+  return m_ballDetector.GetValue() > 1200;
+}
+
+
+double Robot::DetermineShooterAngleFromTargetPosition( double targetPosition )
+{
+  // TODO : determine this equation.
+  double const hoodPosition = targetPosition * ( -150000 ) / ( 20.0 ); 
+  return hoodPosition;
+}
+
+
+void Robot::StopShooterAngle()
+{
+  if ( m_hoodAngleCalFinished )
+  {
+    m_shooterAngle.Set( TalonSRXControlMode::PercentOutput, 0.0 );
+  }
+}
+
+void Robot::ChangeShooterAngle( double position )
+{
+  // Change this so that 0 = bottom, 1 = top.
+  //double const positionScale = 1.0; // TODO : determine this scale.
+  //position = std::clamp( position, 0.0, 1.0 );
+  if ( m_hoodStop.Get() )
+  {
+    m_shooterAngle.SetSelectedSensorPosition(0, 0, 10);
+  }
+
+  position = std::clamp( position, kMinHoodPosition, kMaxHoodPosition  );
+
+  m_shooterAngle.Set( ControlMode::MotionMagic, position /* positionScale*/ );
+}
+
+
+
+void Robot::CalibrateShooterAngle()
+{
+  // Check if the bottom hood stop has been reached.
+  if ( m_hoodStop.Get() )
+  {
+    // If it is reached, stop motor, zero position, and end calibration.
+    m_shooterAngle.Set( TalonSRXControlMode::PercentOutput, 0.0 );
+    // Zero Position.
+    m_shooterAngle.SetSelectedSensorPosition( 0, 0, 10 );
+    m_hoodAngleCalFinished = true;
+  }
+  else
+  {
+    // TODO : What speed should this be.
+    m_shooterAngle.Set( TalonSRXControlMode::PercentOutput, 0.2 );
+  } 
+}
+
+
+
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
---
+++ src/main/include/Robot.h
@@ -0,0 +1,121 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <string>
+
+#include "frc/smartdashboard/Smartdashboard.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableValue.h"
+#include "wpi/span.h"
+
+#include <frc/controller/PIDController.h>
+#include <frc/drive/MecanumDrive.h>
+#include <frc/motorcontrol/PWMVictorSPX.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/AnalogInput.h>
+#include <frc/DigitalInput.h>
+#include <frc/TimedRobot.h>
+#include <frc/XboxController.h>
+
+#include <ctre/Phoenix.h>
+#include <rev/CANSparkMax.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+
+  void SetIntakeMoveSpeed( double );
+  void MoveIntakeOut();
+  void MoveIntakeIn();
+  bool IntakeMovingInward();
+  bool IsBallDetected();
+  double DetermineShooterAngleFromTargetPosition( double );
+  void StopShooterAngle();
+  void ChangeShooterAngle( double );
+  void CalibrateShooterAngle();
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string    kAutoNameDefault = "Default";
+  const std::string    kAutoNameCustom = "My Auto";
+  std::string          m_autoSelected;
+  bool                 m_hoodAngleCalFinished{false};
+
+  std::shared_ptr<nt::NetworkTable> limelightNetworkTable;
+
+  // TODO : Update these PID values
+  double               kRotateP{ 0.05 };
+  double               kRotateI{ 0.0001 };
+  double               kRotateD{ 0.0 };
+
+  double               kMaxHoodPosition{ 0 };
+  double               kMinHoodPosition{ -150000 };
+  frc2::PIDController  m_rotatePid{ kRotateP, kRotateI, kRotateD };
+
+
+
+  frc::XboxController  m_driverController{0};
+
+  // PWM Channels
+  // 0 - Intake, in-out
+  // 1 - Front Right Wheel
+  // 2 - Indexer
+  // 3 - Front Left Wheel
+  // 4 - Rear Right Wheel
+  // 5 - Rear Left Wheel
+  frc::PWMVictorSPX m_intakeMove     {0};
+  frc::PWMVictorSPX m_wheelFrontRight{1};
+  frc::PWMVictorSPX m_indexer        {2}; 
+  frc::PWMVictorSPX m_wheelFrontLeft {3};
+  frc::PWMVictorSPX m_wheelRearRight {4};
+  frc::PWMVictorSPX m_wheelRearLeft  {5};
+
+  frc::MecanumDrive m_drive{ 
+    m_wheelFrontLeft,
+    m_wheelRearLeft,
+    m_wheelFrontRight,
+    m_wheelRearRight };
+
+  // CAN IDs
+  // 0 - Roborio
+  // 1 - Shooter Angle
+  // 2 - Shooter Wheel
+  // 3 - Climber
+  // 4 - Intake Wheels
+  // 5 - PDP
+  TalonSRX         m_shooterAngle{1};
+  rev::CANSparkMax m_shooter     {2, rev::CANSparkMaxLowLevel::MotorType::kBrushless };
+  VictorSPX        m_climber     {3}; 
+  VictorSPX        m_intakeSpin  {4}; 
+
+  // DIO Channels
+  // 0 - Out Stop
+  // 1 - In Stop
+  // 2 - Shooter Hood Stop
+  frc::DigitalInput m_intakeOutStop{0};
+  frc::DigitalInput m_intakeInStop {1};
+  frc::DigitalInput m_hoodStop     {2};
+
+  // Analog Channels
+  // 0 - Ball Detection
+  frc::AnalogInput m_ballDetector{0};
+
+
+  rev::SparkMaxPIDController   m_pidController = m_shooter.GetPIDController();
+  rev::SparkMaxRelativeEncoder m_encoder       = m_shooter.GetEncoder();
+
+};
